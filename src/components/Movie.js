e5c02dd8fcfbfbf3ff63b3870f1dd19163cec4dd
Ankit Kumar <32348430+ankitkumardev25@users.noreply.github.com>
2023-09-09 18:18:38 +0000

    Update 5 files

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 const DEFAULT_PLACEHOLDER_IMAGE =
-  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+  "https://www.subscription.co.uk/time/europe/Solo/Content/Images/noCover.gif";
 
 const Movie = ({ movie }) => {
   const poster =
